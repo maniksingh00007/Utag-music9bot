71e2a9b8812c564c128f2a7934639dba50f7b06d
strad-dev131 <stradiogamer@gmail.com>
2025-07-15 12:38:21 +0530

    New stream.py

--- TEAMXMUSIC/utils/stream/stream.py
+++ TEAMXMUSIC/utils/stream/stream.py
@@ -6,12 +6,12 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from TEAMXMUSIC import Carbon, YouTube, app
-from TEAMXMUSIC.core.call import Siddu
+from TEAMXMUSIC.core.call import JARVIS
 from TEAMXMUSIC.misc import db
 from TEAMXMUSIC.utils.database import add_active_video_chat, is_active_chat
 from TEAMXMUSIC.utils.exceptions import AssistantErr
 from TEAMXMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from TEAMXMUSIC.utils.pastebin import SidduBin
+from TEAMXMUSIC.utils.pastebin import JARVISBin
 from TEAMXMUSIC.utils.stream.queue import put_queue, put_queue_index
 from TEAMXMUSIC.utils.thumbnails import get_thumb
 
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Siddu.force_stop_stream(chat_id)
+        await JARVIS.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Siddu.join_call(
+                await JARVIS.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await SidduBin(msg)
+            link = await JARVISBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(
+            await JARVIS.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(chat_id, original_chat_id, file_path, video=None)
+            await JARVIS.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(chat_id, original_chat_id, file_path, video=status)
+            await JARVIS.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Siddu.join_call(
+            await JARVIS.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(
+            await JARVIS.join_call(
                 chat_id,
                 original_chat_id,
                 link,
