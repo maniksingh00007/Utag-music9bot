25783dba7c1de9c1b1ef58e64a33af0421d32d57
strad-dev131 <stradiogamer@gmail.com>
2025-08-30 13:04:53 +0530

    Update Youtube.py

--- TEAMXMUSIC/platforms/Youtube.py
+++ TEAMXMUSIC/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 import base64
-from TEAMXMUSICXMusic import LOGGER
-from TEAMXMUSICXMusic.utils.database import is_on_off
-from TEAMXMUSICXMusic.utils.formatters import time_to_seconds
+from TEAMXMusic import LOGGER
+from TEAMXMusic.utils.database import is_on_off
+from TEAMXMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
